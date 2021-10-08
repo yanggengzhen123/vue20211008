7578cd0d2bcc44b9693eb70c138d66088e9c68b1
yanggengzhen <15017290625@163.com>
2021-10-09 00:18:43 +0800

    手写vue第三次提交

--- compile.js
+++ compile.js
@@ -3,71 +3,93 @@
 // 判断节点类型，如果是文本，则判断是否是插值绑定
 // 如果是元素，则遍历其属性判断是否是指令或事件，然后递归子元素
 class Compiler {
-    // el是宿主元素 vm是vue实例
-    constructor(el, vm) {
-        this.$vm = vm
-        this.$el = document.querySelector(el)
-        if (this.$el) {
-            // 执行编译
-            this.compile(this.$el)
-        }
+  // el是宿主元素 vm是vue实例
+  constructor(el, vm) {
+    this.$vm = vm;
+    this.$el = document.querySelector(el);
+    if (this.$el) {
+      // 执行编译
+      this.compile(this.$el);
     }
-    compile(el) {
-        // 遍历el树
-        const childNodes = el.childNodes;
-        Array.from(childNodes).forEach(node => {
-            // 判断是否是元素
-            if (this.isElement(node)) {
-                console.log('编译元素' + node.nodeName)
-                this.compileElement(node)
-            } else if (this.isInter(node)) {
-                console.log("编译插值绑定" + node.textContent);
-                // node.textContent 从 {{counter}} 变成vm相对应的值
-                this.compileText(node)
-            }
-            // 如果是元素的话，还需要考虑递归子节点
-            // 递归子节点
-            if (node.childNodes && node.childNodes.length > 0) {
-                this.compile(node)
-            }
-        })
-    }
-    // node.nodeType === 1是元素，node.nodeType === 3是文本
-    isElement(node) {
-        return node.nodeType === 1;
-    }
-    isInter(node) {
-        // 首先是文本标签，其次内容是{{xxx}} \是转义符 .代表若干字符,*代表若干个,()表示到时候可以通过RegExp.$1拿出来
-        return node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textContent)
-    }
-    compileText(node) {
-        node.textContent = this.$vm[RegExp.$1]
-    }
-    compileElement(node) {
-        //节点是元素
-        // 遍历其属性列表
-        const nodeAttrs = node.attributes
-        Array.from(nodeAttrs).forEach(attr => {
-            // 规定:指令以z-xx="oo"定义
-            const attrName = attr.name; //z-xx
-            const exp = attr.value //oo
-            if (this.isDirective(attrName)) { //判断是否是指令(是否是z-开头)
-                const dir = attrName.substring(2) //xx
-                // 执行指令
-                this[dir] && this[dir](node, exp)
-            }
+  }
+  compile(el) {
+    // 遍历el树
+    const childNodes = el.childNodes;
+    Array.from(childNodes).forEach((node) => {
+      // 判断是否是元素
+      if (this.isElement(node)) {
+        console.log("编译元素" + node.nodeName);
+        this.compileElement(node);
+      } else if (this.isInter(node)) {
+        console.log("编译插值绑定" + node.textContent);
+        // node.textContent 从 {{counter}} 变成vm相对应的值
+        this.compileText(node);
+      }
+      // 如果是元素的话，还需要考虑递归子节点
+      // 递归子节点
+      if (node.childNodes && node.childNodes.length > 0) {
+        this.compile(node);
+      }
+    });
+  }
+  // node.nodeType === 1是元素，node.nodeType === 3是文本
+  isElement(node) {
+    return node.nodeType === 1;
+  }
+  isInter(node) {
+    // 首先是文本标签，其次内容是{{xxx}} \是转义符 .代表若干字符,*代表若干个,()表示到时候可以通过RegExp.$1拿出来
+    return node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textContent);
+  }
+  compileText(node) {
+    // node.textContent = this.$vm[RegExp.$1];
+    this.update(node, RegExp.$1, "text");
+  }
+  //公共更新函数
+  update(node, exp, dir) {
+    // 1、初始化操作 node.textContent = this.$vm[RegExp.$1];
+    // 指令对应更新函数xxupdater
+    const fn = this[dir + "Updater"];
+    fn && fn(node, this.$vm[exp]);
 
-        })
-    }
-    isDirective(attr) {
-        return attr.indexOf('z-') === 0
-    }
-    // z-text
-    text(node, exp) {
-        node.textContent = this.$vm[exp]
-    }
-    // z-html
-    html(node, exp) {
-        node.innerHTML = this.$vm[exp]
-    }
+    // 更新处理 =>创建Watcher实例  第三个参数是更新函数(封装一个更新函数，可以更新对应dom元素)
+    new Watcher(this.$vm, exp, function (val) {
+      fn && fn(node, val);
+    });
+  }
+  textUpdater(node, value) {
+    node.textContent = value;
+  }
+  compileElement(node) {
+    //节点是元素
+    // 遍历其属性列表
+    const nodeAttrs = node.attributes;
+    Array.from(nodeAttrs).forEach((attr) => {
+      // 规定:指令以z-xx="oo"定义
+      const attrName = attr.name; //z-xx
+      const exp = attr.value; //oo
+      if (this.isDirective(attrName)) {
+        //判断是否是指令(是否是z-开头)
+        const dir = attrName.substring(2); //xx
+        // 执行指令
+        this[dir] && this[dir](node, exp);
+      }
+    });
+  }
+  isDirective(attr) {
+    return attr.indexOf("z-") === 0;
+  }
+
+  // z-text
+  text(node, exp) {
+    this.update(node, exp, "text");
+    // node.textContent = this.$vm[exp];
+  }
+  // z-html
+  html(node, exp) {
+    this.update(node, exp, "html");
+    // node.innerHTML = this.$vm[exp];
+  }
+  htmlUpdater(node, value) {
+    node.innerHTML = value;
+  }
 }
--- zhenVue.js
+++ zhenVue.js
@@ -1,68 +1,81 @@
 // 创建构造函数
 class zhenVue {
-    constructor(option) {
-        this.$option = option
-        this.$data = option.data
-        // 响应化处理
-        observe(this.$data)
-        // 代理 this.$data.xx ==> this.xx
-        proxy(this, '$data')
-        // 创建编译器
-        new Compiler(option.el, this)
-    }
+  constructor(option) {
+    this.$option = option;
+    this.$data = option.data;
+    // 响应化处理
+    observe(this.$data);
+    // 代理 this.$data.xx ==> this.xx
+    proxy(this, "$data");
+    // 创建编译器
+    new Compiler(option.el, this);
+  }
 }
 // 根据对象类型决定如何做响应化
 class Observer {
-    constructor(value) {
-        this.value = value;
-        // 判断类型(是对象还是数组)
-        if (typeof value === 'object') {
-            this.walk(value)
-        }
+  constructor(value) {
+    this.value = value;
+    // 判断类型(是对象还是数组)
+    if (typeof value === "object") {
+      this.walk(value);
     }
-    // 对象数据响应化
-    walk(obj) {
-        Object.keys(obj).forEach(key => {
-            defineReactive(obj, key, obj[key])
-        })
-    }
-    // 数组数据响应化（待补充）
-
+  }
+  // 对象数据响应化
+  walk(obj) {
+    Object.keys(obj).forEach((key) => {
+      defineReactive(obj, key, obj[key]);
+    });
+  }
+  // 数组数据响应化（待补充）
 }
 function observe(obj) {
-    if (typeof obj !== 'object' || obj == null) return //希望传入的是obj
-    // 创建Observer实例（根据对象类型决定如何做响应化）
-    new Observer(obj)
+  if (typeof obj !== "object" || obj == null) return; //希望传入的是obj
+  // 创建Observer实例（根据对象类型决定如何做响应化）
+  new Observer(obj);
 }
 // 响应式
 function defineReactive(obj, key, val) {
-    observe(val)
-    Object.defineProperty(obj, key, {
-        get() {
-            console.log('get ' + key);
-            return val
-        },
-        set(newVal) {
-            if (newVal !== val) {
-                observe(newVal) //新值是对象的情况
-                console.log('set ' + key + ':' + newVal);
-                val = newVal
-                return val
-            }
-        }
-    })
+  observe(val);
+  Object.defineProperty(obj, key, {
+    get() {
+      console.log("get " + key);
+      return val;
+    },
+    set(newVal) {
+      if (newVal !== val) {
+        observe(newVal); //新值是对象的情况
+        console.log("set " + key + ":" + newVal);
+        val = newVal;
+        return val;
+      }
+    },
+  });
 }
 // 代理函数 this.$data.xx ==> this.xx
-function proxy(vm, sourceKey) { //sourceKey $data
-    Object.keys(vm[sourceKey]).forEach(key => {
-        // 将$data中的key代理到vm属性中
-        Object.defineProperty(vm, key, {
-            get() {
-                return vm[sourceKey][key]
-            },
-            set(newVal) {
-                vm[sourceKey][key] = newVal
-            }
-        })
-    })
+function proxy(vm, sourceKey) {
+  //sourceKey $data
+  Object.keys(vm[sourceKey]).forEach((key) => {
+    // 将$data中的key代理到vm属性中
+    Object.defineProperty(vm, key, {
+      get() {
+        return vm[sourceKey][key];
+      },
+      set(newVal) {
+        vm[sourceKey][key] = newVal;
+      },
+    });
+  });
+}
+// 观察者：保存更新函数，值发生变化调用更新函数
+const watchers = [];
+class Watcher {
+  constructor(vm, key, updateFn) {
+    this.vm = vm;
+    this.key = key;
+    this.updateFn = updateFn;
+    watchers.push(this);
+  }
+  update() {
+    this.updateFn.call(this.vm, this.vm[this.key]);
+  }
 }
